0bf01f073b05c155806988082ab4f44669afbc43
Doug Devine <dougdevine@gmail.com>
2025-04-30 09:38:22 -0400

    eased logic comprehension

--- adjustments_and_estimates.py
+++ adjustments_and_estimates.py
@@ -20,7 +20,7 @@ def _(pl):
     return (df,)
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(pl):
     def clean_source(df, dataset_type):
         return (
@@ -56,7 +56,7 @@ def _(clean_source, df):
     return (pha,)
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(pha, pl, raw, tob):
     combined = (
         pha
@@ -73,100 +73,99 @@ def _(pha, pl, raw, tob):
             suffix='_tob'
         )
         .sort(['year', 'month', 'coop_id'])
-        .select(['year', 'month', 'coop_id', 'temp', 'dmflag', 'temp_raw', 'dmflag_raw', 'temp_tob', 'dmflag_tob'])
+        .rename({'temp': 'temp_pha', 'dmflag': 'dmflag_pha'})
+        .select(['year', 'month', 'coop_id', 'temp_raw', 'temp_tob', 'temp_pha', 'dmflag_raw', 'dmflag_tob', 'dmflag_pha'])
         .with_columns(
-            (pl.col('temp_tob') - pl.col('temp_raw')).alias('adjustment_tob'),
-            (pl.col('temp') - pl.col('temp_tob')).alias('adjustment_pha'),
-            (pl.col('temp') - pl.col('temp_raw')).alias('adjustment'),
+            # Determine the adjustment
+            (pl.col('temp_tob') - pl.col('temp_raw')).alias('tob_adjustment'),
+            (pl.col('temp_pha') - pl.col('temp_tob')).alias('pha_adjustment'),
+            # Add dropped flags
+            (pl.col('temp_tob').is_null() & pl.col('temp_raw').is_not_null()).alias('tob_dropped'),
+            (pl.col('temp_pha').is_null() & pl.col('temp_tob').is_not_null()).alias('pha_dropped'),
+        )
+        .with_columns(
+            # Add adjusted flags
+            (pl.col('tob_adjustment') != 0).alias('tob_adjusted'),
+            (pl.col('pha_adjustment') != 0).alias('pha_adjusted'),
         )
     )
-    combined
     return (combined,)
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(combined, pl):
     stats = (
         combined
-        # .filter(
-        #     pl.col('month') == 7
-        # )
         .group_by('year')
         .agg(
             # Number of observations
-            pl.col('temp').is_not_null().sum().alias('n_obs_pha'),
-            pl.col('temp_tob').is_not_null().sum().alias('n_obs_tob'),
-            pl.col('temp_raw').is_not_null().sum().alias('n_obs_raw'),
-        
+            pl.col('temp_raw').count().alias('n_raw'),
+            pl.col('temp_tob').count().alias('n_tob'),
+            pl.col('temp_pha').count().alias('n_pha'),
+
             # Number of dropped observations
-            pl.col('temp_raw').filter(pl.col('temp_tob').is_null()).count().alias('n_tob_dropped_obs'),
-            pl.col('temp_tob').filter(pl.col('temp').is_null()).count().alias('n_pha_dropped_obs'),
-        
-            # # Number of temp. observations that exist is both raw and altered data
-            # pl.col('adjustment_tob').count().alias('n_obs_fboth'),
-        
-            # # Number of temp. observations that exist is both raw and altered data that have been altered
-            # pl.col('adjustment').filter(pl.col('adjustment') != 0).count().alias('n_adjusted_obs'),
-        
-            # Number of temp. observations that have been estimated
-            pl.col('temp').filter(
-                (pl.col('dmflag') == 'E') & (pl.col('temp').is_not_null())
-            ).count().alias('n_obs_estimated'),
+            pl.col('tob_dropped').sum().alias('n_tob_dropped'),
+            pl.col('pha_dropped').sum().alias('n_pha_dropped'),
 
             # Number of obs that have been adjusted
-            pl.col('temp_tob').filter(pl.col('adjustment_tob') != 0).count().alias('n_tob_adjusted'),
-            pl.col('temp').filter(pl.col('adjustment_pha') != 0).count().alias('n_pha_adjusted'),
-            pl.col('temp').filter(pl.col('adjustment') != 0).count().alias('n_adjusted'),
-        
+            pl.col('tob_adjusted').sum().alias('n_tob_adjusted'),
+            pl.col('pha_adjusted').sum().alias('n_pha_adjusted'),
+                
             # Average adjusment
-            pl.col('adjustment_tob').filter(pl.col('adjustment_tob') != 0).mean().alias('avg_tob_adjustment'),
-            pl.col('adjustment_pha').filter(pl.col('adjustment_pha') != 0).mean().alias('avg_pha_adjustment'),
-            pl.col('adjustment').filter(pl.col('adjustment') != 0).mean().alias('avg_adjustment'),
+            pl.col('tob_adjustment').mean().alias('avg_tob_adjustment'),
+            pl.col('pha_adjustment').mean().alias('avg_pha_adjustment'),
+        
+            # Number of temp. observations that have been estimated
+            pl.col('temp_pha').filter((pl.col('dmflag_pha') == 'E')).count().alias('n_estimated'),
         
             # Average temp. of obs. that were estimated
-            pl.col('temp').filter(pl.col('dmflag') == 'E').mean().alias('avg_estimate'),
-        
+            pl.col('temp_pha').filter(pl.col('dmflag_pha') == 'E').mean().alias('avg_estimated_temp'),
+
             # Average raw temp. of obs. that were not estimated
             pl.col('temp_raw').filter(
-                (pl.col('dmflag') != 'E') 
-                | pl.col('dmflag').is_null()
-            ).mean().alias('avg_non_estimated_raw'),
+                (pl.col('dmflag_pha') != 'E') 
+                | pl.col('dmflag_pha').is_null()
+            ).mean().alias('avg_non_estimated_raw_temp'),
         )
         .with_columns(
-            (100 * pl.col('n_tob_adjusted') / pl.col('n_obs_raw')).alias('percent_tob_adjusted'),
-            (100 * pl.col('n_pha_adjusted') / pl.col('n_obs_tob')).alias('percent_pha_adjusted'),
-            (100 * pl.col('n_adjusted') / pl.col('n_obs_raw')).alias('percent_adjusted'),
-            (pl.col('avg_estimate') - pl.col('avg_non_estimated_raw')).alias('avg_estimate_delta'),
+            (100 * pl.col('n_tob_adjusted') / pl.col('n_raw')).alias('percent_tob_adjusted'),
+            (100 * pl.col('n_pha_adjusted') / pl.col('n_tob')).alias('percent_pha_adjusted'),
+            (pl.col('avg_estimated_temp') - pl.col('avg_non_estimated_raw_temp')).alias('avg_estimate_delta'),
         )
     )
     return (stats,)
 
 
-@app.cell
-def _(stats):
-    stats
-    return
-
-
-@app.cell
+@app.cell(hide_code=True)
 def _(pl, plt):
-    def make_chart(df: pl.DataFrame, line_field: str , bar_field: str, title: str):
+    def make_chart(df: pl.DataFrame, line_field_1: str, line_field_2: str , bar_field: str, title: str):
         left_y_axis_color = 'blue'
         right_y_axis_color = 'green'
 
-        if line_field.startswith('percent'):
+        if line_field_1.startswith('percent'):
             y_min = 0
+            y1_label = '% of Observations Adjusted'
         else:
             y_min = None
+            y1_label = 'Temp. Delta (F)'
 
+        if bar_field == 'n_raw':
+            y2_label = '# of Raw Observations'
+        elif bar_field == 'n_estimated':
+            y2_label = '# of Estimated Observations'
+        else:
+            y2_label = bar_field
+        
         # Create the figure and axis
         fig, ax = plt.subplots(figsize=(9, 5))
 
-        line = ax.plot(df['year'], df[line_field], color=left_y_axis_color, linewidth=2, label=line_field)[0]
-
+        line_1 = ax.plot(df['year'], df[line_field_1], color=left_y_axis_color, linewidth=1, label=line_field_1)[0]
+        if line_field_2:
+            line_2 = ax.plot(df['year'], df[line_field_2], color=left_y_axis_color, linewidth=1, label=line_field_2, linestyle='--')[0]
+    
         # Set labels and title for the left y-axis
         plt.xlabel('Year')
-        plt.ylabel(line_field, color=left_y_axis_color)
+        plt.ylabel(y1_label, color=left_y_axis_color)
         plt.tick_params(axis='y', labelcolor=left_y_axis_color)
         ax.set_ylim(y_min, None)  # Set left y-axis minimum to 0
         plt.title(title)
@@ -174,15 +173,19 @@ def _(pl, plt):
 
         # Create a second y-axis for the bar plot
         ax2 = plt.twinx()
+    
         # Plot bars 
         bars = ax2.bar(df['year'], df[bar_field], color=right_y_axis_color, alpha=0.5, label=bar_field, width=0.4)
 
         # Set labels for the right y-axis
-        ax2.set_ylabel(bar_field, color=right_y_axis_color)
+        ax2.set_ylabel(y2_label, color=right_y_axis_color)
         ax2.tick_params(axis='y', labelcolor=right_y_axis_color)
 
         # Combine legends from both axes
-        plt.legend([line, bars], [line_field, bar_field], loc='upper left')
+        if line_field_2:
+            plt.legend([line_1, line_2, bars], [line_field_1, line_field_2, bar_field], loc='upper left')
+        else:
+            plt.legend([line_1, bars], [line_field_1, bar_field], loc='upper left')
 
         # Adjust layout
         plt.tight_layout()
@@ -193,25 +196,19 @@ def _(pl, plt):
 
 @app.cell
 def _(make_chart, stats):
-    make_chart(stats, 'percent_adjusted', 'n_obs_raw', "When raw data already existed, here's how frequently it was adjusted")
+    make_chart(stats, 'percent_tob_adjusted', 'percent_pha_adjusted', 'n_raw', "When raw data already existed, here's how frequently it was adjusted")
     return
 
 
 @app.cell
 def _(make_chart, stats):
-    make_chart(stats, 'avg_adjustment', 'n_obs_raw', "When raw data already existed, here's how it was adjusted")
+    make_chart(stats, 'avg_tob_adjustment', 'avg_pha_adjustment', 'n_raw', "When raw data already existed, here's how it was adjusted")
     return
 
 
 @app.cell
 def _(make_chart, stats):
-    make_chart(stats, 'avg_tob_adjustment', 'n_obs_raw', "When raw data already existed, here's how it was adjusted for TOD Bias")
-    return
-
-
-@app.cell
-def _(make_chart, stats):
-    make_chart(stats, 'avg_estimate_delta', 'n_obs_estimated', "When raw data did not exist, here's how it was estimated\n(relative to the average of existing raw data)")
+    make_chart(stats, 'avg_estimate_delta', '', 'n_estimated', "When raw data did not exist, here's how it was estimated\n(relative to the average of existing raw data)")
     return
 
 
